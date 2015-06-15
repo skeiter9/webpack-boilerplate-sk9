84a477fc64d87636a2a1a4094a1669329e8e7182
skeiter9 <skeiter97@gmail.com>
2015-06-15 16:31:08 -0500

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 var path = require('path');
 var Webpack = require('webpack');
 
-var buildPath = path.resolve(__dirname, 'public', 'build');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var srcPath = path.join(__dirname, 'src');
+var buildPath = path.join(__dirname, 'build');
 
 var npmPath = path.join(__dirname, 'node_modules');
 var bowerPath = path.join(__dirname, 'bower_components');
-var appPath = path.join(__dirname, 'app');
 
 var DEV = JSON.parse(process.env.BUILD_DEV || 'true');
 var DEPLOY = JSON.parse(process.env.BUILD_DEPLOY || 'false');
@@ -13,56 +15,63 @@ var DEPLOY = JSON.parse(process.env.BUILD_DEPLOY || 'false');
 var config = {
   addVendor: function(name, path_) {
     this.resolve.alias[name] = path_;
-    this.module.noParse.push(new RegExp(path_));
+    this.module.noParse.push(new RegExp(path_))
   },
-  devtool: 'eval',
+  context: srcPath,
+  debug: true,
+  devtool: 'cheap-source-map',
   entry: {
     app: [
       'webpack/hot/dev-server',
-      'webpack-dev-server/client?http://localhost:8080',
-      './app/app'
+      'webpack-dev-server/client?http://localhost:' +
+      (process.env.PORT || 8081),
+      './app'
     ]
   },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: buildPath,
     publicPath: '/build/',
-    chunkFilename: '[name]-[id]-[chunkhash].js'
+    chunkFilename: '[id]-chunk.js',
+    pathinfo: true
   },
   module: {
     noParse: [],
     preLoaders: [
-      {
-        test: /src\/([a-z_-]*\/|[a-z_-]+)\.js$/,
-        loader: 'jscs'
-      }
+      {test: /\.js$/, loaders: ['jscs'],
+        exclude: /node_modules|bower_components|api-lb/},
+      {test: /\.js$/, loaders: ['source-map']}
     ],
     loaders: [
-      {
-        test: /\.js$/,
-        loader: 'ng-annotate!babel',
-        include: /app\//,
-        exlude: [/node_modules|bower_components/]
-      },
-      {test: /\.jade$/, loader: 'jade'},
       {test: /\.css$/, loader: 'style!css'},
-      {test: /\.scss$/, loader: 'style!css!sass'},
-      {test: /\.styl$/, loader: 'style!css!stylus'},
-      {test: /\.(png|jpg|jpeg|woff|woff2)$/, loader: 'url', query: {limit: 100000, name: '[name].[ext]'}}
+      {test: /\.styl$/, loader: 'style!css?sourceMap!stylus?sourceMap'},
+      {test: /\.scss$/, loader: ExtractTextPlugin.extract('style',
+        'css?sourceMap!sass?sourceMap&sourceComments&includePaths[]=' +
+        bowerPath)},
+      {test: /\.jade$/, loader: 'jade'},
+      {test: /\.(png|jpg|jpeg)$/,
+          loader: 'url?limit=20000&name=assets/[name].[ext]!img?' +
+            'minimize&progressive=true'},
+      {test: /\.(woff|woff2)$/, loader: 'url?limit=100000&name=[name].[ext]'}
     ]
   },
   plugins: [
     new Webpack.DefinePlugin({
-      __DEV__: DEV,
-      __DEPLOY__: DEPLOY
+      __DEVELOPMENT__: DEV,
+      __PRODUCTION__: DEPLOY
     }),
-    new Webpack.HotModuleReplacementPlugin()
-    //new Webpack.optimize.CommonsChunkPlugin({
-      //name: 'vendor',
-      //filename: 'vendor.bundle.js'
-    //})
+    new Webpack.ProvidePlugin({
+      angular: 'exports?window.angular!angular/angular.min'
+    }),
+    new Webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: 'vendor.bundle.js',
+      minSize: 50
+    }),
+    new ExtractTextPlugin('[name].css')
   ],
   resolve: {
+    root: [path.join(__dirname, 'app')],
     alias: [],
     extensions: [
       '',
@@ -75,9 +84,23 @@ var config = {
   }
 };
 
-if (DEPLOY) {
-  config.entry.app = './app/app';
-  config.devtool = 'source-map'
+if (DEV) {
+  config.plugins.unshift(new Webpack.HotModuleReplacementPlugin());
 }
+
+if (DEPLOY) {
+
+  config.entry.app = './app';
+  config.debug = false;
+  config.devtool = 'source-map';
+  config.output.pathinfo = false;
+
+  config.plugins.push(new Webpack.optimize.UglifyJsPlugin());
+  config.plugins.push(new Webpack.optimize.MinChunkSizePlugin(100));
+
+}
+
+//config.addVendor('angularMin',
+//  npmPath + '/angular/angular.min');
 
 module.exports = config;
